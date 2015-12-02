54b69794a785997cfdceecc3906c09814253c446
vlashton <vlashton@ashtonfam.org>
2015-12-02 11:22:37 -0500

    16x16 Grid

--- 9007199254740992_files/application.js
+++ 9007199254740992_files/application.js
@@ -2,5 +2,5 @@ var GM;
 
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  GM = new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  GM = new GameManager(16, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
